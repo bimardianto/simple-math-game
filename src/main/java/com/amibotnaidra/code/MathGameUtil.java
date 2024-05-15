25026cbd6fee29c7c67ea53ab94af3d704a62ce9
bimardianto <bimaa289@gmail.com>
2024-05-15 09:56:20 +0700

    rename class

--- src/main/java/com/amibotnaidra/code/MathGameUtil.java
+++ src/main/java/com/amibotnaidra/code/MathGameUtil.java
@@ -2,7 +2,7 @@ package com.amibotnaidra.code;
 
 import java.util.Scanner;
 
-public class InputUtil {
+public class MathGameUtil {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String input(String info) {
@@ -22,6 +22,27 @@ public class InputUtil {
             }
         }
         return number;
+    }
+
+    public static boolean checkAnswer(String answer, String correctAnswer, int question, int score, int lives, final int MAX_QUESTION) {
+        if (answer.equals(correctAnswer)) {
+            score += 2;
+            System.out.println("Hooray... correct! your score is " + score + " (lives " + lives + ")");
+        } else {
+            score -= 2;
+            lives--;
+            System.out.println("Sorry, wrong answer. Your score is " + score + " (lives: " + lives + ")");
+            if (lives == 0) {
+                System.out.println("GAME OVER");
+                System.out.println("Score: " + score);
+                return false;
+            }
+        }
+        if (question == (MAX_QUESTION - 1)) {
+            System.out.println("Thank you for playing! Your score is " + score);
+            return false;
+        }
+        return true;
     }
 
 }
